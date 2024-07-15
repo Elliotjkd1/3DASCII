ce8b1040f9e8e695d447da5aef1a7c2f256c2a78
elliotjkd <elliotjkd@gmail.com>
2024-07-15 17:38:40 -0500

    convert image to ascii through text file

--- convert.py
+++ convert.py
@@ -2,8 +2,40 @@ from PIL import Image, ImageDraw, ImageFont
 
 import math
 
-image = Image.open("bmw.jpg")
-width, height = image.size
-pix = image.load()
+chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "[::-1]
+# chars = "#Wo- "[::-1]
+charArray = list(chars)
+charLength = len(charArray)
+interval = charLength/256
 
-print(width, height)
+scaleFactor = 0.09
+
+oneCharWidth = 10
+oneCharHeight = 18
+
+def getChar(inputInt):
+    return charArray[math.floor(inputInt*interval)]
+
+text_file = open("Output.txt", "w")
+
+im = Image.open("bmw.jpg")
+
+width, height = im.size
+im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(oneCharWidth/oneCharHeight))), Image.NEAREST)
+width, height = im.size
+pix = im.load()
+
+outputImage = Image.new('RGB', (oneCharWidth * width, oneCharHeight * height), color = (0, 0, 0))
+d = ImageDraw.Draw(outputImage)
+
+for i in range(height):
+    for j in range(width):
+        r, g, b = pix[j, i]
+        h = int(r/3 + g/3 + b/3)
+        pix[j, i] = (h, h, h)
+        text_file.write(getChar(h))
+        d.text((j*oneCharWidth, i*oneCharHeight), getChar(h), fill = (r, g, b))
+
+    text_file.write('\n')
+
+outputImage.save('output.png')
